8f5f55b7969a55b1301258e87290696ad42a5f9e
Matheus Limão Brites <majortheus@gmail.com>
2025-04-09 19:32:28 -0300

    level 06 class done

---
+++ 06-design-system/packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,29 @@
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/majortheus.png',
+    alt: 'Matheus Limão',
+  },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {
+  args: {},
+}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+    alt: 'Fallback',
+  },
+}
---
+++ 06-design-system/packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,45 @@
+import { Text, TextProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    size: 'md',
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus asperiores corrupti soluta illum tenetur ex libero quasi mollitia corporis, iusto perferendis esse aspernatur quis beatae voluptatibus sint tempora commodi quia?',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {
+  args: {},
+}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
